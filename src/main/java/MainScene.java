44bfb15c3dd3a1924bb43aee15192dd66bb09974
W-Fewix <felix.wang3780@gmail.com>
2019-06-06 09:59:55 -0400

    Update MainScene.java

--- src/main/java/MainScene.java
+++ src/main/java/MainScene.java
@@ -1,31 +1,32 @@
-//
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+// creates main scene for boggle GUI
+// using javaFX
+import javafx.geometry.Pos;                                                                         // importing required libraries
+import javafx.scene.Scene;                                                                          // importing required libraries
+import javafx.scene.control.Button;                                                                 // importing required libraries
+import javafx.scene.layout.VBox;                                                                    // importing required libraries
+import javafx.scene.text.Font;                                                                      // importing required libraries
+import javafx.scene.text.Text;                                                                      // importing required libraries
 
 public class MainScene {
 
     public static Scene getScene() {
-        BoggleGUI.stage.setTitle("Boggle Game");
+        BoggleGUI.stage.setTitle("Boggle Game");                                                    // naming the window Boggle Game
 
-        Text t = new Text();
-        t.setFont(new Font(18));
-        t.setText("Welcome to Boggle!");
+        Text t = new Text();                                                                        // creating text box
+        t.setFont(new Font(18));                                                                    // setting font size
+        t.setText("Welcome to Boggle!");                                                            // giving user a welcome message
 
-        Button play = new Button("Play!"), settings = new Button("Settings");
-        play.setDefaultButton(true);
-        play.setOnMouseClicked((e) -> {
-            SetupScene.currentPlayer = 0;
-            BoggleGUI.stage.setScene(SetupScene.getScene());
+        Button play = new Button("Play!"), settings = new Button("Settings");                       // creating 2 buttons
+        play.setDefaultButton(true);                                                                //
+        play.setOnMouseClicked((e) -> {                                                             // checking mouse button click
+            SetupScene.currentPlayer = 0;                                                           // starts the setup with the first player
+            BoggleGUI.stage.setScene(SetupScene.getScene());                                        // change scene if Play! button is pressed
         });
-        settings.setOnMouseClicked(e -> BoggleGUI.stage.setScene(SettingsScene.getScene()));
+        settings.setOnMouseClicked(e -> BoggleGUI.stage.setScene(SettingsScene.getScene()));        // change scene if Settings button is pressed
 
-        VBox pane = new VBox(t, new Text(), play, new Text(), settings);
-        pane.setAlignment(Pos.CENTER);
-        return new Scene(pane, 256, 256);
+        VBox pane = new VBox(t, new Text(), play, new Text(), settings);                            // creating verticle GUI layout for text, play button, and settings button
+        pane.setAlignment(Pos.CENTER);                                                              // aligning GUI to center
+        return new Scene(pane, 256, 256);                                                           // setting the size of the scene as 256x256 pixels
     }
 
 }
