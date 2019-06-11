7a75b80248bc827dc9705420d60b772f4cb75695
W-Fewix <felix.wang3780@gmail.com>
2019-06-11 00:38:38 -0400

    Update Player.java

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,38 +2,38 @@ import java.util.HashSet;
 
 public class Player implements Comparable<Player> {
 
-    private int score;
-    private String name;
-    private HashSet<String> usedWords;
+    private int score;                                      // creating variable to store individual player score
+    private String name;                                    // creating variable to store individual player name
+    private HashSet<String> usedWords;                      // creating HashSet to store each individual players used words
 
     public Player (String name) {
-        this.name = name;
-        score = 0;
-        usedWords = new HashSet<>();
+        this.name = name;                                   // initializes player name
+        score = 0;                                          // initializes player score
+        usedWords = new HashSet<>();                        // initializes player usedwords
     }
 
     public int getScore() {
-        return score;
+        return score;                                       // returns players score when called
     }
 
     public String getName() {
-        return name;
+        return name;                                        // returns players name when called
     }
 
     public boolean isUsedWord(String str) {
-        return usedWords.contains(str);
+        return usedWords.contains(str);                     // returns players list of used words when called
     }
 
     public void setScore(int score) {
-        this.score = score;
+        this.score = score;                                 // sets players score when called
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name = name;                                   // sets players name when called
     }
 
     public void addUsedWord(String word) {
-        usedWords.add(word);
+        usedWords.add(word);                                // sets players used words when called (adds the word)
     }
 
     @Override
